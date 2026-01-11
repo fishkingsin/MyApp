ff2d1bd240c8664dffa2a246f33aa1f7efb5476a
James Kong <fishkingsin@gmail.com>
2026-01-11 21:55:44 +0800

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by James Kong on 11/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
